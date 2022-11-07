700c9a9eedd05fed119047a8bea00af27b00f7ec
transon <transon1901@gmail.com>
2022-11-07 12:15:03 +0700

    add

--- src/controller/siteController.js
+++ src/controller/siteController.js
@@ -5,15 +5,6 @@ const getHomePage=(req,res)=>{
     connection.query(
     'SELECT * FROM `users`',
     function(err, results, fields) {
-      //  results.map((row)=>{
-      //    data.push({
-      //       id:row.id,
-      //       firstName:row.firstName,
-      //       lastName:row.lastName,
-      //       adress:row.adress
-      //    })
-      // })
-      // console.log(data)
       return res.render('home',{data:results})
    }
 );
